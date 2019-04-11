fc61dc7217a7189a7cc89bf28f2c82e2257aa93a
wildnez <rahul@hazelcast.com>
2019-04-11 12:52:08 +0300

    Initial commit

---
+++ taxitrajectory/src/main/java/com/hazelcast/demo/iot/data/TaxiData.java
@@ -0,0 +1,29 @@
+package com.hazelcast.demo.iot.data;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class TaxiData implements Serializable {
+
+    private String ID;
+    private Timestamp timestamp;
+    private Location location;
+
+    public TaxiData(String ID, Timestamp timestamp, Location location) {
+        this.ID = ID;
+        this.timestamp = timestamp;
+        this.location = location;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+}
